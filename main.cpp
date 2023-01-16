54d220f8a4e45febd8d8e27e686e0bdc467b11fb
radicalDilettante <wonjun92@gmail.com>
2023-01-16 20:24:07 +1300

    Interpolation

--- main.cpp
+++ main.cpp
@@ -32,23 +32,28 @@ static const char* vShader = "                                                \n
 #version 330                                                                  \n\
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
+																			  \n\
+out vec4 vCol;																  \n\
                                                                               \n\
 uniform mat4 model;                                                           \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
     gl_Position = model * vec4(pos, 1.0);									  \n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0);								  \n\
 }";
 
 // Fragment Shader
 static const char* fShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
+in vec4 vCol;																  \n\
+                                                                              \n\
 out vec4 colour;                                                              \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
+    colour = vCol;															  \n\
 }";
 
 void CreateTriangle()
@@ -237,9 +242,9 @@ int main()
 		glm::mat4 model(1.0f);
 
 		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 		
-		model = glm::scale(model, glm::vec3(curSize, 0.4f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.0f));
 
 
 
