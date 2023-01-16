00bdc5aea4423b18703d6eefb60e4640c338e39a
radicalDilettante <wonjun92@gmail.com>
2023-01-16 20:19:33 +1300

    Transforming: Translation

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VBO, VAO, shader, uniformXMove;
+GLuint VBO, VAO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -21,11 +25,11 @@ static const char* vShader = "                                                \n
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
                                                                               \n\
-uniform float xMove;                                                          \n\
+uniform mat4 model;                                                           \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);				  \n\
+    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		  \n\
 }";
 
 // Fragment Shader
@@ -123,7 +127,7 @@ void CompileShaders()
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -203,7 +207,10 @@ int main()
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
